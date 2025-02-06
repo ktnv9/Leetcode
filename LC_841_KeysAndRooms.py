0cffa351e5ae931671e2f0e35d3a730c8e8cdd9e
ktnv9 <ktnvarma@gmail.com>
2025-02-06 14:50:12 +0530

    Optimized the code using set data structure

--- LC_841_KeysAndRooms.py
+++ LC_841_KeysAndRooms.py
@@ -1,41 +1,30 @@
 def canVisitAllRooms(rooms) -> bool:
     
-    # initially none of the rooms are visited.
-    visited = {}
-    for i in range(len(rooms)):
-        visited[i] = False
+    # mark the first room visited.
+    visited = {0}
 
-    # mark the first room as visited.
+    # stack to traverse the graph of rooms.
     stack = [0]
-    visited[0] =  True
 
-    while not stack == []:
+    while stack:
 
-        # go to the recently visited room
-        top = stack[-1]
+        # recently visited room.
+        room = stack[-1]
+        
+        # get keys and search for the unvisited room.
+        unv_room_found = False
+        for key in rooms[room]:
+            if key not in visited:
 
-        # get the keys.
-        keys = rooms[top]
-
-        # identify the unvisited room using the available keys.
-        unvisited_room = -1
-        for key in keys:
-            if not visited[key]:
-                unvisited_room = key
+                # if unvisited room is found, mark it visited.
+                stack.append(key)
+                visited.add(key)
+                unv_room_found = True
                 break
-                    
-        # if an unvisited room exists, mark it as visited.
-        if unvisited_room > 0:
-            stack.append(unvisited_room)
-            visited[unvisited_room] = True
-        
-        # if all rooms for which the keys are available are visited then continue with the next recently visited room.
-        else:
-            stack.pop()
 
+        # if all keys from recent room are all visited, proceed with the next recent room.
+        if not unv_room_found:
+            stack.pop()
+    
     # check if all rooms are visited.
-    for room in visited:
-        if not visited[room]:
-            return False
-        
-    return True
+    return len(visited) == len(rooms)
