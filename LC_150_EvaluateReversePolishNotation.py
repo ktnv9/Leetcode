c5baa714edbabe01a6c82af589508f23ef142e3c
ktnv9 <ktnvarma@gmail.com>
2025-02-14 15:11:48 +0530

    LC 150 | Added comments

--- LC_150_EvaluateReversePolishNotation.py
+++ LC_150_EvaluateReversePolishNotation.py
@@ -1,19 +1,32 @@
 def evaluateRPN(tokens):
 	
+	# use stack to efficiently keep track of numbers and operators.
 	stack = []
 	
 	for token in tokens:
 		if token == "+":
+
 			stack.append(stack.pop() + stack.pop())
+
 		elif token == "-":
+
+			# note the order of pops.
 			b, a = stack.pop(), stack.pop()
 			stack.append(a-b)
+
 		elif token == "*":
+
 			stack.append(stack.pop() * stack.pop())
+
 		elif token == "/":
+
+			# note the order of pops.
 			b, a = stack.pop(), stack.pop()
 			stack.append(int(a/b))
+			
 		else:
+
+			# important to convert the input token to int.
 			stack.append(int(token))
 
 	return stack[0]
