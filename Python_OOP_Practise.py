03d3f1b3d9fdca5f75d5c3990f29ccf35e734423
ktnv9 <ktnvarma@gmail.com>
2025-03-04 21:56:02 +0530

    LC 141 & Python OOP Concepts

--- Python_OOP_Practise.py
+++ Python_OOP_Practise.py
@@ -1,3 +1,129 @@
+# OOP Principles
+
+
+
+# Encapsulation
+    # bundling data & methods.
+
+'''
+class BadBankAccount:
+
+    def __init__(self, balance):
+        self.balance = balance
+
+    
+account = BadBankAccount(0)
+account.balance = -1
+
+print(account.balance)
+
+
+class BankAccount:
+
+    def __init__(self):
+        self._balance = 0.0
+
+    @property
+    def balance(self):
+        return self._balance
+    
+    def deposit(self, amount):
+        if amount <= 0:
+            raise ValueError("Deposit amount must be positive")
+        self._balance += amount
+
+    def withdraw(self, amount):
+        if amount <= 0:
+            raise ValueError("Withdraw amount must be positive")
+        if amount > self._balance:
+            raise ValueError("Insufficient funds")
+        self._balance -= amount
+
+account = BankAccount()
+print(account.balance)
+
+account.deposit(199)
+print(account.balance)
+
+account.withdraw(12)
+print(account.balance)
+
+account.withdraw(255j)
+print(account.balance)
+
+
+class EmailService:
+
+    def _connect(self):
+        print("connecting to email server")
+
+    def _authenticate(self):
+        print("authenticating...")
+
+    def send_email(self):
+        self._connect()
+        self._authenticate()
+        print("sending email...")
+        self._disconnect()
+
+    def _disconnect(self):
+        print("Disconnecting from email server.")
+
+
+email = EmailService()
+email.send_email()
+'''
+
+class Vehicle:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+
+    def start(self):
+        print("Vehicle is starting")
+
+    def stop(self):
+        print("Vehicle is stopping")
+
+class Car(Vehicle):
+    
+    def __init__(self, brand, model, year, door_count, wheel_count):
+        super().__init__(brand, model, year)
+        self.door_count = door_count
+        self.wheel_count = wheel_count
+
+class Bike(Vehicle):
+
+    def __init__(self, brand, model, year, wheel_count):
+        super().__init__(brand, model, year)
+        self.wheel_count = wheel_count
+
+car = Car("Ford", "Focus", 2008,5,4)
+bike = Bike("Honda", "Scoopy", 2015, 2)
+
+print(car.__dict__)
+print(bike.__dict__)
+
+car.start()
+bike.start()
+
+car.stop()
+bike.stop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 '''
 # black formatter, pylance, python debugger, python
 
@@ -50,7 +176,7 @@ class Person:
 person = Person("Varma", 30)
 person.Greet()
 '''
-
+'''
 class User:
 
     def __init__(self, name, email):
@@ -131,7 +257,7 @@ account.deposit(200) # instance method
 print(BankAccount.is_valid_interest_rate(4)) # static method
 print(BankAccount.is_valid_interest_rate(10)) # static method
 print(account.is_valid_interest_rate(9))
-
+'''
 
 
     
